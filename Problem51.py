569f6b97d10754a15863e48f34b2a96f6eb93911
Rohan Mitta <mitta.rohan@gmail.com>
2023-12-02 15:11:08 +0800

    began Problem51

--- Problem51.py
+++ Problem51.py
@@ -2,21 +2,45 @@
 #Find the smallest prime which, by replacing part of the number (not necessarily adjacent digits) with the same digit, is part of an eight prime value family.
 
 #
+from itertools import combinations
+from line_profiler import LineProfiler
+import numpy as np
 
 def replaceDigits(num,digit,positions):
-    digitList = [int(x) for x in str(num)]
-    newDigitList = [digit if index in positions else digitList[index] for index in range(len(digitList))]
-    newStringDigitList = [str(digit) for digit in newDigitList]
-    return int("".join(newStringDigitList))
+    numStr = str(num)
+    for position in positions:
+        numStr = numStr[:position] + digit + numStr[1+position:]
+    return int(numStr)
 
 def digitReplaceFamily(num,positions):
-    return [replaceDigits(num,digit,positions) for digit in range(1,10)]
+    return [replaceDigits(num,str(digit),positions) for digit in range(10)]
 
-def allDigitReplaceFamilies(num):
-    for numChanges in range(1,len(num)+1):
-        
+def enumeratePositionOptions(numLength):
+    onlyMultiplesOf3 = True
+    if onlyMultiplesOf3:
+        numChangeRange = range(3,numLength+1,3)
+    else:
+        numChangeRange = range(1,numLength+1)
+    changes = []
+    for numChanges in numChangeRange:
+        changes += list(combinations(range(numLength),numChanges))
+    return changes
 
+def primesBitArray(primesList,maxN):
+    bitArray = np.zeros((maxN+1), dtype=np.bool_)
+    for p in primesList:
+        bitArray[p] = True
+    return bitArray
 
+def checkPrimesList(primesList,maxN):
+    bitArray = primesBitArray(primesList,maxN)
+    for prime in primesList:
+        for positions in enumeratePositionOptions(len(str(prime))):
+            family = digitReplaceFamily(prime,positions)        #This takes all the time now.
+            if len([x for x in family if bitArray[x]]) == 8:
+                if len(str(family[0])) == len(str(prime)):
+                    return family
+    return "No 8 prime family found in primesList"
 
 import math
 def primesUpTo(n):
@@ -26,6 +50,20 @@ def primesUpTo(n):
     while len(potentialPrimes) > 0 :
         p = potentialPrimes.pop(0)
         primes.append(p)
-        if p < sqrtn:
+        if p <= sqrtn:
             potentialPrimes = [x for x in potentialPrimes if (x % p != 0)]
     return primes
+
+
+#checkPrimesList(primesUpTo(100000))
+
+lp = LineProfiler()
+lp.add_function(replaceDigits)
+lp_wrapper = lp(checkPrimesList)
+lp_wrapper(primesUpTo(10000000),10000000)
+lp.print_stats()
+
+#This family [2090021, 2191121, 2292221, 2393321, 2494421, 2595521, 2696621, 2797721, 2898821, 2999921] works but isn't the smallest I guess? Unless my primes list is wrong
+
+#Just realised we can reduce our search in some way by using the fact that if there are two or one changePositions, then you cannot have 3 consectuve digit replacemenets (one must be divisible by three because it rotates through mod 3)
+#This would imply that the most in a prime family would be 0,1,_,3,4,_,6,7,_,9 i.e. 7 primes. To get 8 primes, we must have the number of changepositions being a multiple of 3.
